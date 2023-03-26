a517ef51e807d9bf1a53cd32ef3fe71af9ce2cb8
Jane42070 <jql1377219787@gmail.com>
2023-03-26 23:05:02 +0800

    mod file mode 644

--- config.h
+++ config.h
@@ -46,7 +46,7 @@ static const char delimiter[] = {' ', DELIMITERENDCHAR };
 
 static Block blocks[] = {
 /*      pathu                        pathc    interval        signal */
-		{ PATH("songs"),             PATH("songs"),        10,           2},
+		{ PATH("songs"),             PATH("songs"),         0,           2},
 		{ PATH("weather"),           PATH("weather"),       0,           8},
         { PATH("packages"),          PATH("packages"),      0,           9},
 		{ PATH("meminfo"),           PATH("meminfo"),       5,           3},
